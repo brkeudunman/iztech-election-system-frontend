c11d5d895f7970211360b8d8c32be113fbacc7b9
Berke Udunman <berkeudunman@gmail.com>
2023-05-16 21:54:49 +0300

    navbar

--- src/pages/public/index.js
+++ src/pages/public/index.js
@@ -1,11 +1,29 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./homepage/home";
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
+const { Header, Content, } = Layout;
 
 const PublicRoutes = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />}></Route>
-    </Routes>
+    <Layout>
+      <Header
+        style={{
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <div className="demo-logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={["2"]}
+          
+        />
+      </Header>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </Layout>
   );
 };
 export default PublicRoutes;
