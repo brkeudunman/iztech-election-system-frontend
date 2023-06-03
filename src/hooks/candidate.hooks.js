ed24625d69cd78bb0397f4f0eba4f79c80be263e
sezinserficeli <sezinserficeli@std.iyte.edu.tr>
2023-06-03 14:11:35 +0300

    candidate hooks change

--- src/hooks/candidate.hooks.js
+++ src/hooks/candidate.hooks.js
@@ -1,5 +1,11 @@
-import { useQuery } from "react-query";
-import { getAllCandidates } from "./../api/candidate/candidate.api";
+import { useMutation, useQuery } from "react-query";
+import {
+  getAllCandidates,
+  deleteCandidate,
+  updateCandidate,
+  getCandidate,
+  addCandidate,
+} from "./../api/candidate/candidate.api";
 import { notification } from "antd";
 
 export const useGetAllCandidates = (onSuccess) => {
@@ -13,6 +19,78 @@ export const useGetAllCandidates = (onSuccess) => {
       onError: (error) => {
         notification.error({
           description: "Error while receiving candidates",
+          message: error.message,
+        });
+      },
+    }
+  );
+};
+
+export const useDeleteCandidates = (onSuccess, id) => {
+  return useMutation(
+    "delete-candiate",
+    (data) => {
+      return deleteCandidate(id, data);
+    },
+    {
+      onSuccess,
+      onError: (error) => {
+        notification.error({
+          description: "Error while deleting candidate",
+          message: error.message,
+        });
+      },
+    }
+  );
+};
+
+export const useUpdateCandidate = (onSuccess, id) => {
+  return useMutation(
+    "update-candidate",
+    (data) => {
+      return updateCandidate(id, data);
+    },
+    {
+      onSuccess,
+      onError: (error) => {
+        notification.error({
+          description: "Error while updating candidate",
+          message: error.message,
+        });
+      },
+    }
+  );
+};
+
+export const useGetCandidate = (onSuccess) => {
+  return useQuery(
+    "get-candidate",
+    (id) => {
+      return getCandidate(id);
+    },
+    {
+      onSuccess,
+      onError: (error) => {
+        notification.error({
+          description: "Error while receiving candidate",
+          message: error.message,
+        });
+      },
+    }
+  );
+};
+
+export const useAddCandidate = (onSuccess, id) => {
+  return useMutation(
+    "add-candidate",
+    (data) => {
+      return addCandidate(id, data);
+    },
+    {
+      onSuccess,
+      onError: (error) => {
+        notification.error({
+          description: "Error while sending candidate",
           message: error.message,
         });
       },
