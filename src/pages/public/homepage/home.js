78068eefb1911283c8145c311b3e12f59d76a6ae
sezinserficeli <sezinserficeli@std.iyte.edu.tr>
2023-06-07 17:10:08 +0300

    public home fix

--- src/pages/public/homepage/home.js
+++ src/pages/public/homepage/home.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
+import { Col, Typography, Row } from "antd";
+
+const { Title } = Typography;
 
 const Home = () => {
   return (
     <div>
-        Home
+      <Row justify={"center"}>
+        <Col span={8}>
+          <Title level={2}>Welcome to IZTECH Election System</Title>
+        </Col>
+      </Row>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
